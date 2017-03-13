68d44b8b4a0381aad2e60f4d88a00141af9f0ddb
FreeFly19 <wmsystem2008@yandex.ru>
2017-03-13 19:57:18 +0200

    Added spring jdbc

---
+++ src/dump.sql
@@ -0,0 +1,6 @@
+create database java_course;
+create table java_course.students (
+  id int auto_increment primary key,
+  name text,
+  age int
+);
--- src/main/java/springexample/dao/StudentDao.java
+++ src/main/java/springexample/dao/StudentDao.java
@@ -1,36 +1,36 @@
 package springexample.dao;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import springexample.Student;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDao {
-    public List<Student> getAllStudents() throws IOException {
-        byte[] jsonBytes = Files.readAllBytes(new File("d:/students.txt").toPath());
-        String json = new String(jsonBytes);
+    private JdbcTemplate jdbcTemplate;
 
-        Gson gson = new Gson();
-        Type listOfStudents = new TypeToken<ArrayList<Student>>(){}.getType();
-
-        return gson.fromJson(json, listOfStudents);
+    public StudentDao() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://127.0.0.1/java_course");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void saveStudent(Student student) throws IOException {
-        List<Student> allStudents = getAllStudents();
-        allStudents.add(student);
 
-        String json = new Gson().toJson(allStudents);
+    public List<Student> getAllStudents() {
+        List<Student> students = jdbcTemplate
+                .query("select * from students",
+                        new BeanPropertyRowMapper(Student.class));
 
-        FileOutputStream fileOS = new FileOutputStream("D:/students.txt");
-        fileOS.write(json.getBytes());
-        fileOS.close();
+        return students;
+    }
+
+    public void saveStudent(Student student) {
+        jdbcTemplate.update(
+                "INSERT INTO students(`name`, `age`) VALUES (?, ?)",
+                    student.getName(), student.getAge()
+        );
     }
 }
